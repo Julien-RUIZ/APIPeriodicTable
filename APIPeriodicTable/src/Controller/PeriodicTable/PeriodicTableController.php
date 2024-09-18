7383f0e8a08c0196b39ac82e483942b92a9f3552
Julien RUIZ <julien.ruiz@hotmail.fr>
2024-09-18 17:01:22 +0200

    updating Front and definition

--- APIPeriodicTable/src/Controller/PeriodicTable/PeriodicTableController.php
+++ APIPeriodicTable/src/Controller/PeriodicTable/PeriodicTableController.php
@@ -4,6 +4,7 @@ namespace App\Controller\PeriodicTable;
 
 use App\Interface\ElementHelperInterface;
 use App\Repository\ElementCategoryRepository;
+use App\Repository\ElementDefinitionsRepository;
 use App\Repository\ElementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,21 +19,28 @@ class PeriodicTableController extends AbstractController
     }
 
     #[Route('/tableau/{param}={value}', name: 'app_tableau', requirements: ['param' => '\w+', 'value' => '\d+'], defaults: ['param' => null, 'value' => null])]
-    public function index(Request $request, ElementRepository $atomeRepository, ElementHelperInterface $elementHelper, ElementCategoryRepository $categoryRepository): Response
+    public function index(ElementDefinitionsRepository $definitionsRepository, Request $request, ElementRepository $elementRepository, ElementHelperInterface $elementHelper, ElementCategoryRepository $categoryRepository): Response
     {
         $category = $categoryRepository->findAll();
         $param = $request->attributes->get('param');
         $value = $request->attributes->get('value');
+        $def=[];
 
         if ($param === null && $value === null){
-            $this->ListeElements = $atomeRepository->findAll();
+            $this->ListeElements = $elementRepository->findAll();
             foreach ($this->ListeElements as $value){
                 $this->listefamily[] = $elementHelper->LanthanidesActinides($value->getId());
                 $this->Elements[$value->getId()] = $value;
             }
         }
         if ($param !== null && $value !== null){
-            $this->ListeElements = $atomeRepository->findBy([$param=>$value]);
+
+            $def= $definitionsRepository->findOneBy(['name'=>$param]);
+
+
+
+
+            $this->ListeElements = $elementRepository->findBy([$param=>$value]);
             foreach ($this->ListeElements as $value){
                 $this->listefamily[] = $elementHelper->LanthanidesActinides($value->getId());
                 $this->Elements[$value->getId()] = $value;
@@ -41,7 +49,7 @@ class PeriodicTableController extends AbstractController
         $this->Lanthanides = array_search('Lanthanides', $this->listefamily);
         $this->Actinides = array_search('Actinides', $this->listefamily);
         return $this->render('table/index.html.twig', [
-            'Elements' => $this->Elements, 'Lanthanides'=>$this->Lanthanides, 'Actinides'=>$this->Actinides, 'category'=>$category
+            'Elements' => $this->Elements, 'Lanthanides'=>$this->Lanthanides, 'Actinides'=>$this->Actinides, 'category'=>$category, 'def'=>$def
         ]);
     }
 }
